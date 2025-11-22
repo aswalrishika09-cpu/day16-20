1fed70cc78251e1fcc7cf0d2b937f126621219fb
dorime <aswalrishika09@gmail.com>
2025-11-22 18:39:56 +0530

    Create day 18.36.c

---
+++ day 18.36.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Euclidean algorithm
+    while (num2 != 0) {
+        temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+    }
+
+    printf("HCF (GCD) = %d\n", num1);
+
+    return 0;
+}
