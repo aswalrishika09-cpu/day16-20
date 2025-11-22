1585b01a7b78c930946c7e3e5ae7d6fd7e8c5734
dorime <aswalrishika09@gmail.com>
2025-11-22 18:42:00 +0530

    Create day 19.37.c

---
+++ day 19.37.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2, hcf, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    hcf = gcd(num1, num2);                 
+    lcm = (num1 * num2) / hcf;             
+
+    printf("LCM of %d and %d = %d\n", num1, num2, lcm);
+
+    return 0;
+}
